64ef36b24c41b493bbaf367cbcb51428ed2f8e92
Max Heilmann <heilmann@matix-media.net>
2024-09-17 19:35:38 +0200

    test: :white_check_mark: update test to check all cafeterias

--- test/meals.test.ts
+++ test/meals.test.ts
@@ -1,11 +1,37 @@
 import { expect, test } from "vitest";
 import MealsAPI, { MealLocation } from "../src";
 
-test("get meals for monday of this week", async () => {
+function getMonday() {
     const d = new Date();
     var day = d.getDay(),
         diff = d.getDate() - day + (day == 0 ? -6 : 1); // adjust when day is sunday
-    const monday = new Date(d.setDate(diff));
-    const meals = await MealsAPI.getMeals(monday, monday, MealLocation.Elbe);
-    expect(meals.length).toBeGreaterThan(0);
+    return new Date(d.setDate(diff));
+}
+
+test("get meals for 'Elbe' monday of this week", async () => {
+    const monday = getMonday();
+
+    const mealsElbe = await MealsAPI.getMeals(monday, monday, MealLocation.Elbe);
+    expect(mealsElbe.length).toBeGreaterThan(0);
+});
+
+test("get meals for 'Steelrunner' monday of this week", async () => {
+    const monday = getMonday();
+
+    const mealsSteelrunner = await MealsAPI.getMeals(monday, monday, MealLocation.Steelrunner);
+    expect(mealsSteelrunner.length).toBeGreaterThan(0);
+});
+
+test("get meals for 'Bonprix' monday of this week", async () => {
+    const monday = getMonday();
+
+    const mealsBonprix = await MealsAPI.getMeals(monday, monday, MealLocation.Bonprix);
+    expect(mealsBonprix.length).toBeGreaterThan(0);
+});
+
+test("get meals for 'Boulevard' monday of this week", async () => {
+    const monday = getMonday();
+
+    const mealsBoulevard = await MealsAPI.getMeals(monday, monday, MealLocation.Boulevard);
+    expect(mealsBoulevard.length).toBeGreaterThan(0);
 });
